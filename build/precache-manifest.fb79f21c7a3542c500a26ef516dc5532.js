fa1293fd0b22d67163203c844626dccad02b322b
Juan <jupegarnica@gmail.com>
2018-10-04 12:27:38 +0200

    build created

---
+++ build/precache-manifest.fb79f21c7a3542c500a26ef516dc5532.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d3c178cdcd4164dac5b0",
+    "url": "/static/js/main.d3c178cd.chunk.js"
+  },
+  {
+    "revision": "2113394fe1475efe9560",
+    "url": "/static/js/1.2113394f.chunk.js"
+  },
+  {
+    "revision": "d3c178cdcd4164dac5b0",
+    "url": "/static/css/main.6a1afec2.chunk.css"
+  },
+  {
+    "revision": "e04f7c5b63b64b2d19a68b76113e0862",
+    "url": "/index.html"
+  }
+];
