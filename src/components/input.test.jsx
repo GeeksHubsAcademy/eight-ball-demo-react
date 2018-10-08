7c3a32b9b5222e07665374352cb3291e8c0ff34a
Juan <jupegarnica@gmail.com>
2018-10-08 12:26:23 +0200

    testing with enzyme configured

---
+++ src/components/input.test.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { shallow } from "enzyme";
+import Input from './Input';
+
+it('renders without crashing', () => {
+    const wrapper = shallow(<Input />);
+    expect(wrapper).toMatchSnapshot();
+});
