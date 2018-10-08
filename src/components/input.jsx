7d02367ee1f92b48ab8d38d8b2818ab60a6874d6
Juan <jupegarnica@gmail.com>
2018-10-08 11:00:54 +0200

    empty input and output component

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,27 +1,7 @@
 import React from "react";
 import './input.scss';
 export default class Input extends React.Component {
-  state = {
-    question: ""
-  };
-  submit = ev => {
-    ev.preventDefault();
-    if (this.state.question && typeof this.props.submit === "function") {
-        this.props.submit(this.state.question);
-        this.setState({ question: '' })
-    }
-  };
   render() {
-    return (
-      <form onSubmit={this.submit} className="Input">
-        <input
-          type="text"
-          placeholder="¿Qué quieres saber?"
-          value={this.state.question}
-          onChange={event => this.setState({ question: event.target.value })}
-        />
-        <button type="submit">Dime!</button>
-      </form>
-    );
+    return 'input';
   }
 }
--- src/components/output.jsx
+++ src/components/output.jsx
@@ -2,18 +2,6 @@ import React from "react";
 import "./output.scss";
 export default class Output extends React.Component {
   render() {
-    return (
-      <div className="Output">
-        {this.props.history.map((record, i) => (
-          <div
-            className={(i === 0 ? "last" : "" )+ ` record ${record.answer.type}`}
-            key={i}
-          >
-            <div className="question"> ¿{record.question}? </div>
-            <div className="answer">{record.answer.text}</div>
-          </div>
-        ))}
-      </div>
-    );
+    return 'output';
   }
 }
