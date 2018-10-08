1f6b1008d2908f9a51a93985083426e9f5170c3e
Juan <jupegarnica@gmail.com>
2018-10-08 13:34:51 +0200

    eject , config enzyme and test input component

--- src/components/input.test.jsx
+++ src/components/input.test.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React from "react";
 
-import { shallow } from "enzyme";
-import Input from './Input';
+import { shallow, mount } from "enzyme";
+import Input from "./Input";
 
-it('renders without crashing', () => {
-    const wrapper = shallow(<Input />);
-    expect(wrapper).toMatchSnapshot();
+it("renders without crashing", () => {
+  shallow(<Input />);
+});
+
+it("It has one input in a form", () => {
+  const wrapper = shallow(<Input />);
+  const form = wrapper.find("form");
+  expect(form).toHaveLength(1);
+  expect(form.find("input")).toHaveLength(1);
+});
+
+it("It shouldn't change", () => {
+  const wrapper = shallow(<Input />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+it("should recieve a submit function", () => {
+  const wrapper = mount(<Input submit={console.log} />);
+  expect(typeof wrapper.prop("submit")).toBe("function");
+});
+
+it("should run the submit function onSubmit event", () => {
+  const onSubmit = jest.fn();
+  const value = "test text";
+  const wrapper = mount(<Input submit={onSubmit} />);
+  wrapper
+    .find("input")
+    .at(0)
+    .simulate("change", {
+      preventDefault: () => {},
+      target: { value }
+    });
+  wrapper
+    .find("form")
+    .at(0)
+    .simulate("submit", { preventDefault: () => {} });
+  expect(onSubmit).toHaveBeenCalled();
+  expect(onSubmit).toBeCalledWith(value);
 });
